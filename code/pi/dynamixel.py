a532606cc5849c52fc74ada269d47ffb106aae00
Hendrik Ohrdes <devdrik@gmail.com>
2021-05-13 11:13:10 +0200

    refactored byte handling when reading angle

--- code/pi/dynamixel.py
+++ code/pi/dynamixel.py
@@ -17,18 +17,10 @@ class Dynamixel(IServo):
 
     def getAngle(self):
         function = 2
-        self.ser.flush()
         self.ser.write("{func}:{id}\n".format(func=function, id=self.id).encode("utf-8"))
-        self.ser.flush()
-        myByte = []
-        for k in range(4):
-            myByte.append(int.from_bytes(self.ser.read(1), byteorder='big', signed=False))
-            print(f'byte[{k}]={myByte[k]}')
-        self.ser.flush()
-        angle= struct.unpack('<f', bytearray(myByte))[0]
-        print(f'floatAngle={angle}')
+        readBytes = self.ser.read(4)
+        angle= struct.unpack('<f', readBytes)[0]
 
-        # angle = int.from_bytes(self.ser.read(1), byteorder='big', signed=False)
         correctedAngle = angle - 180
         log("angle read from servo {} is {} and should be {}".format(self.id, correctedAngle, self.goalAngle))
         return correctedAngle
@@ -38,23 +30,16 @@ class Dynamixel(IServo):
         correctedAngle = angle + 180
         self.goalAngle = correctedAngle
         log("setting {} to {}".format(self.id,correctedAngle))
-        self.ser.flush()
         self.ser.write("{func}:{id}:{ang:.2f}\n".format(func=function, id=self.id, ang=correctedAngle).encode("utf-8"))
-        self.ser.flush()
         # sleep(0.01)
 
     def setProfileVelocity(self, velocity):
         function = 4
-        print("writing velocity: ", velocity)
-        self.ser.flush()
         self.ser.write("{func}:{id}:{vel}\n".format(func=function, id=self.id, vel=velocity).encode("utf-8"))
 
     def hasReachedAngle(self):
-        # hasReached = self.goalAngle - self.offset  <= self.getAngle() <= self.goalAngle + self.offset
         function = 6
-        self.ser.flush()
         self.ser.write("{func}:{id}\n".format(func=function, id=self.id).encode("utf-8"))
-        self.ser.flush()
         hasReached = int.from_bytes(self.ser.read(1), byteorder='big', signed=False)
         if hasReached == 0:
             pass
