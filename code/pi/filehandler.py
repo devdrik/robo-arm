1d49dfd4811a54ed70270d6de57e53c57cb21385
Hendrik Ohrdes <devdrik@gmail.com>
2021-06-21 09:55:52 +0200

    added velocity handling in filehander

--- code/pi/filehandler.py
+++ code/pi/filehandler.py
@@ -2,10 +2,16 @@
 
 class FileHandler():
 
+    def __init__(self):
+        self.velocityIdentifier = 'vel'
+
     def saveToFile(self, fname, angles):
         with open("moveFiles/" + fname, "w") as f:
             for angle in angles:
-                f.write("{},{},{},{},{}\n".format(angle[0],angle[1],angle[2],angle[3],angle[4]))
+                if angle[0] == self.velocityIdentifier:
+                    f.write(f'{angle[0]}:{angle[1]}\n')
+                else:
+                    f.write("{},{},{},{},{}\n".format(angle[0],angle[1],angle[2],angle[3],angle[4]))
 
 
     def getAnglesFromFile(self, fname):
@@ -15,6 +21,11 @@ class FileHandler():
         # print(lines)
         for line in lines:
             lineValues = []
+            if line.startsWith(self.velocityIdentifier):
+                values = line.split(':')
+                lineValues.append(values[0])
+                lineValues.append(float(values[1]))
+                angles.append(lineValues)
             for value in line.split(','):
                 if value.endswith("\n"):
                     value=value[:-1]
