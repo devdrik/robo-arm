10cdd2bd62d8b7fabca30ec5490f4c613d358088
devdrik <67547895+devdrik@users.noreply.github.com>
2021-08-25 22:12:51 +0200

    Chore/decouple actions from cli (#11)
    * decoupled actions from cli
    
    * fixed bugs and error

---
+++ code/pi/actionHandler.py
@@ -0,0 +1,55 @@
+import time
+
+class ActionHandler():
+
+    def __init__(self, robo, fileHandler):
+        self.robo = robo
+        self.fileHandler = fileHandler
+        self.actions = []
+
+    def setPosition(self):
+        action = self.robo.getAngles()
+        self.actions.append(action)
+        print(action)
+
+    def saveToFile(self, fname):
+        self.fileHandler.saveToFile(fname, self.actions)
+
+    def appendFromFile(self, fname):
+        try:
+            actionsFromFile = self.fileHandler.getActionsFromFile(fname)
+            self.actions.extend(actionsFromFile)
+        except:
+            print("Error, file not existent?")
+
+    def runActions(self, velocity):
+        self.robo.startPositionMode()
+        self.robo.setVelocity(velocity)
+        for action in self.actions:
+            if action[0] == 'vel':
+                self.robo.setVelocity(action[1])
+            elif action[0] == 'pause':
+                time.sleep(action[1])
+            else:
+                self.robo.setAnglesBlocking(action)
+                # self.robo.setAngles(angle)
+                # time.sleep(0.1)
+
+    def setVelocity(self, velocity: int):
+        entry = []
+        entry.append('vel')
+        entry.append(velocity)
+        self.actions.append(entry)
+
+    def addPause(self, pauseInSeconds: float):
+        entry = []
+        entry.append('pause')
+        entry.append(pauseInSeconds)
+        self.actions.append(entry)
+
+    def deleteLastEntry(self):
+        self.actions.pop()
+    
+    def showEntries(self):
+        for entry in self.actions:
+            print(entry)
--- code/pi/robocli.py
+++ code/pi/robocli.py
@@ -2,13 +2,11 @@ import time
 
 class RoboCLI():
 
-    def __init__(self, robo, fileHandler):
+    def __init__(self, actionHandler, robo):
+        self.actionHandler = actionHandler
         self.robo = robo
-        self.fileHandler = fileHandler
-        self.actions = []
     
     def startTeachingCli(self):
-        self.actions = []
         while True:
             self.robo.startTeachMode()
             menu = self.__getMenu()
@@ -39,46 +37,24 @@ class RoboCLI():
                 self.__showEntries()
 
     def __setPosition(self):
-        action = self.robo.getAngles()
-        self.actions.append(action)
-        print(action)
+        self.actionHandler.setPosition()
 
     def __saveToFile(self):
         fname = input("enter filename: ")
-        self.fileHandler.saveToFile(fname, self.actions)
+        self.actionHandler.saveToFile(fname)
 
     def __apendFromFile(self):
         fname = input("enter filename: ")
-        try:
-            actionsFromFile = self.fileHandler.getActionsFromFile(fname)
-            self.actions.extend(actionsFromFile)
-        except:
-            print("Error, file not existent?")
+        self.actionHandler.appendFromFile(fname)
 
     def __runActions(self):
-        self.robo.startPositionMode()
-        self.robo.setVelocity(self.__getVelocityInput())
-        for action in self.actions:
-            if action[0] == 'vel':
-                self.robo.setVelocity(action[1])
-            elif action[0] == 'pause':
-                time.sleep(action[1])
-            else:
-                self.robo.setAnglesBlocking(action)
-                # self.robo.setAngles(angle)
-                # time.sleep(0.1)
+        self.actionHandler.runActions(self.__getVelocityInput())
 
     def __setVelocity(self):
-        entry = []
-        entry.append('vel')
-        entry.append(self.__getVelocityInput())
-        self.actions.append(entry)
+        self.actionHandler.setVelocity(self.__getVelocityInput())
 
     def __addPause(self):
-        entry = []
-        entry.append('pause')
-        entry.append(self.__getFloatInput("enter pause in [s]: "))
-        self.actions.append(entry)
+        self.actionHandler.addPause(self.__getFloatInput("enter pause in [s]: "))
 
 
     def __getMenu(self):
@@ -126,8 +102,7 @@ class RoboCLI():
         return val
 
     def __deleteLastEntry(self):
-        self.actions.pop()
+        self.actionHandler.deleteLastEntry()
     
     def __showEntries(self):
-        for entry in self.actions:
-            print(entry)
+        self.actionHandler.showEntries()
